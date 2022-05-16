f5ce049ccc04ab454a8d3b424995b2035f9c2158
TaTamta <t.tavartkiladze@outlook.com>
2022-05-16 16:18:25 +0400

    Update giphy.js
    search and trends work but they only show 1 gif

--- giphy.js
+++ giphy.js
@@ -6,13 +6,36 @@ const trendingsURL = "https://api.giphy.com/v1/gifs/trending?"
 // const searchURL = `https://api.giphy.com/v1/gifs/search?q=${input}&rating=g&api_key=${apiKey}`;
 
 let searchButton = document.getElementById('search_button');
-
+let randomsButton = document.getElementById('trendings_button')
 
 searchButton.addEventListener('click',() => {
-    request()
+    searchRequest()
 })
 
-async function request(){
+randomsButton.addEventListener('click', () => {
+    trendingsRequest()
+})
+
+
+
+async function trendingsRequest(){
+    let trendsResponse = await fetch(`${trendingsURL}api_key=${apiKey}`);
+    console.log(trendsResponse);
+    let trends = await trendsResponse.json()
+    console.log(trends)
+    showData(trends)
+}
+
+function showData(trends){
+    for(let i = 0; i < trends.data.length; i++){
+        let newImg = `<img src="${trends.data[i].images.fixed_height.url}">`;
+        document.querySelector('#results').innerHTML = newImg;
+        
+    }
+}
+
+
+async function searchRequest(){
     const search_word = document.getElementById("input_word").value;
     let response = await fetch(`${giphyUrl}api_key=${apiKey}&q=${search_word}`);
     console.log(response);
@@ -24,8 +47,12 @@ async function request(){
 
 function showData(gifs){
     for(let i = 0; i < gifs.data.length; i++){
-        document.querySelector('#results').innerHTML = `<img src="${gifs.data[i].images.fixed_height.url}">`
+        let newImg = `<img src="${gifs.data[i].images.fixed_height.url}">`;
+        document.querySelector('#results').innerHTML = newImg;
+        
     }
-    
 }
+
+
+
 
